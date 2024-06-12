4882c93be2fc1833a43f6cc2ca7c255c545c7ee4
Aluno <Aluno@LAB04-PC08>
2024-06-11 21:35:08 -0300

    Commit shoppingCart01

---
+++ src/ShoppingCart01.java
@@ -0,0 +1,21 @@
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+        // declaração da variavel
+     String custName;
+     String itemDesc;
+     String mensagem;        
+      
+     //atribuir variavel
+      custName="jailton";
+      itemDesc="camiseta";
+      mensagem= custName+ " muito obrigado por comprar uma "+ itemDesc ;       
+       System.out.println(mensagem); 
+        
+        
+        // Assign the message variable 
+        
+        
+        // Print and run the code
+        
+    }
+}
